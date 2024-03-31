a589836067134c0508c327e6d3f95fdac8a30f95
violet <lvhoa58@gmail.com>
2024-03-31 16:37:01 +0700

    create proj template

---
+++ src/processor.rs
@@ -0,0 +1,14 @@
+// program logic
+
+use solana_program::{account_info::AccountInfo, entrypoint::ProgramResult, pubkey::Pubkey};
+
+pub struct Processor;
+impl Processor {
+    pub fn process(
+        program_id: &Pubkey,
+        accounts: &[AccountInfo],
+        instruction_data: &[u8],
+    ) -> ProgramResult {
+        Ok(())
+    }
+}
